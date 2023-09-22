56a35effec8113d5fd6e430dccdf801f80d81d57
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-22 11:42:03 +0300

    Update deduction_11.cpp

--- argument_deduction/deduction_11.cpp
+++ argument_deduction/deduction_11.cpp
@@ -5,9 +5,9 @@
 template <typename T, typename A, template<typename, typename>typename Con>
 void func(const Con<T, A>& con)
 {
-	std::cout << "T is : " << typeid(T).name() << "\n";
-	std::cout << "A is : " << typeid(A).name() << "\n";
-	std::cout << "Con is : " << typeid(Con).name() << "\n";
+	std::cout << "T is : " << typeid(T).name() << '\n';
+	std::cout << "A is : " << typeid(A).name() << '\n';
+	std::cout << "Con is : " << typeid(Con).name() << '\n';
 }
 
 int main()
