4dfd7f50d829f4a74faa0d0092fb1240e036ca23
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-22 11:56:38 +0300

    Update deduction_09.cpp

--- argument_deduction/deduction_09.cpp
+++ argument_deduction/deduction_09.cpp
@@ -1,11 +1,11 @@
 #include <type_traits>
 #include <iostream>
 
-
+//type_identity 
 template <typename T>
 void func(T x, std::type_identity_t<T>);
 
 int main()
 {
-	func(12, 'A'); // func<int, int>
+	func(12, 'A'); // func<int>
 }
