e2afc5e685e25a84095fb1ba3821000a4e648229
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-28 10:58:03 +0300

    Create es_13.cpp

---
+++ explicit_specialization/es_13.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+template<typename T>
+constexpr std::size_t SZ = sizeof(T);
+
+template<>
+constexpr std::size_t SZ<void> = 0;
+
+int main()
+{
+	std::cout << SZ<double> << '\n';
+	std::cout << SZ<void> << '\n'
+}
