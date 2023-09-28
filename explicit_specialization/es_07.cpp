241a1a6ee03e003f29afa7b6af8c56ca8b6298f1
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-28 10:51:29 +0300

    Create es_07.cpp

---
+++ explicit_specialization/es_07.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+template<typename T>
+void func(T)
+{
+	std::cout << 1;
+}
+
+template<>
+void func(int*)
+{
+	std::cout << 2;
+}
+
+template<typename T>
+void func(T*)
+{
+	std::cout << 3;
+}
+
+int main()
+{
+	int* p = nullptr;
+
+	func(p);
+}
