73b3bea211baf8ae213b01c937ad5cc040d9807c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-28 11:03:27 +0300

    Create ps_05.cpp

---
+++ partial_specialization/ps_05.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+template <typename T>
+struct Myclass {
+	Myclass()
+	{
+		std::cout << "primary template\n";
+	}
+};
+
+template <typename T>
+struct Myclass<T[]> {
+	Myclass()
+	{
+		std::cout << "Myclass<T[]> specialization>\n";
+	}
+};
+
+int main()
+{
+	Myclass<int[5]> m1;
+	Myclass<double[10]> m2;
+	Myclass<int[]> m3;
+	Myclass<double[]> m4;
+}
