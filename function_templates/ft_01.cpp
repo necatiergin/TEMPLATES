e5b0e79c279f525495363789c5e3b3bcf7f6180b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-28 10:15:53 +0300

    Create ft_01.cpp

---
+++ function_templates/ft_01.cpp
@@ -0,0 +1,14 @@
+template<typename T, int N, int M> 
+bool is_less(T(&a)[N], T(&b)[M])
+{
+	for (int i = 0; i < N && i < M; ++i)
+	{
+		if (a[i] < b[i]) 
+			return true; 
+		
+		if (b[i] < a[i]) 
+			return false;
+	} 
+	
+	return N < M;
+}
