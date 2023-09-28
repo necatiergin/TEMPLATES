d7e4cae064ada5d656b2f921db713415278e8822
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-28 18:14:14 +0300

    Create ins_03.cpp

---
+++ instantiation/ins_03.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+template <typename T>
+struct Nec
+{
+	static T ms;
+	
+};
+
+template <typename T> 
+T Nec<T>::ms = 0;
+
+int main()
+{
+	Nec<int> n1;
+	Nec<double> n2;
+	Nec<double> n3;
+
+	std::cout << n1.ms << '\n'; // 0
+	std::cout << n2.ms << '\n'; // 0
+	std::cout << n3.ms << '\n'; // 0
+	
+	n2.ms = 23;
+
+	std::cout << n1.ms << '\n'; // 0
+	std::cout << n2.ms << '\n'; // 23
+	std::cout << n3.ms << '\n'; // 23
+}
