8370debe101d655bd3bd7a27d1c373216788668b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-28 17:20:07 +0300

    Create ins_01.cpp

---
+++ instantiation/ins_01.cpp
@@ -0,0 +1,16 @@
+template <typename T>
+struct Nec
+{
+	void foo() {}
+	void bar() {}
+};
+
+int main()
+{
+	Nec<char>* pcnec; // no instantiatipn
+	Nec<int> nec;  // Nec<int> is instantiated
+	Nec<float>* pfnec; // no instantiation
+	//...
+	nec.foo(); //Nec<int>::foo instantiated
+	pfnec->bar(); //Nec<float> and Nec<float>::bar instantiated
+}
