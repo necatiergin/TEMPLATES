fc8886817b67d37039a83a3d5c0c568a1677a6b1
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-29 09:48:45 +0300

    Create tp_07.cpp

---
+++ template_parameters/tp_07.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+
+
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p)
+{
+	return os << '(' << p.first << ',' << p.second << ')';
+}
+
+template <template <typename...> typename C, typename... Ts>
+std::ostream& operator<<(std::ostream& os, C<Ts...> const& c)
+{
+	for (auto const& e : c)
+	{
+		os << e << ' ';
+	}
+	return os;
+}
+
+
+int main()
+{
+	std::vector ivec{10, 20, 30, 40, 50};
+	std::list ilist{2, 3, 5, 7, 11, 13};
+	std::map<int, int> imap{{1, 4}, {2,7}, {3,8}};
+
+	std::cout << ivec << '\n';
+	std::cout << ilist << '\n';
+	std::cout << imap << '\n'; //valid
+}
