fe928fcb2e703ad12a589aeaceb1b9748843e31c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-30 05:11:28 +0300

    Create at_06.cpp

---
+++ alias_templates/at_06.cpp
@@ -0,0 +1,13 @@
+#include <type_traits>
+
+template <typename T, typename... Args>
+struct nec {};
+
+template <typename... Args>
+using nec_int = nec<int, Args...>;
+
+nec<double, char, int> f1;
+nec<int, char, double> f2;
+nec_int<char, double> f3;
+
+static_assert(std::is_same_v<decltype(f2), decltype(f3)>);
