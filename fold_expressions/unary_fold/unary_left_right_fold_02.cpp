2f8b6ffb91e959896c112ec641cb601420b824a8
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-10 15:27:30 +0300

    Update unary_left_right_fold_02.cpp

--- fold_expressions/unary_fold/unary_left_right_fold_02.cpp
+++ fold_expressions/unary_fold/unary_left_right_fold_02.cpp
@@ -1,10 +1,11 @@
 #include <vector>
+#include <iostream>
 
 // bu örnekte unary right fold olması ile unary left fold olması sonuç açısından bir fark oluturmuyor
-
 //unary right fold
+
 template<typename T, typename... Args>
-void pushback_left(std::vector<T>&v, Args&& ...args)
+void pushback_left(std::vector<T>& v, Args&& ...args)
 {
 	(v.push_back(std::forward<Args>(args)), ...);
 }
@@ -13,17 +14,15 @@ void pushback_left(std::vector<T>&v, Args&& ...args)
 template<typename T, typename... Args>
 void pushback_right(std::vector<T>& v, Args&& ...args)
 {
-	(... , v.push_back(std::forward<Args>(args)));
+	(..., v.push_back(std::forward<Args>(args)));
 }
-
-#include <iostream>
 
 int main()
 {
-	std::vector<int> vec;
+	std::vector<int> vec{};
 	//pushback_left(vec, 2, 3, 5, 7, 11, 13);
 	pushback_right(vec, 2, 3, 5, 7, 11, 13);
 
 	for (auto i : vec)
-		std::cout << i << "\n";
+		std::cout << i << ' ';
 }
