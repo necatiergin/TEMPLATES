4d914093c21afd2ad14ffc73c4da773916930261
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-10 15:42:24 +0300

    Update and rename unary_right_fold_02.cpp to unary_fold_04.cpp

---
+++ fold_expressions/unary_fold/unary_fold_04.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <iostream>
+#include <string>
+
+template<typename C, typename ...Ts>
+auto matches(const C& range, Ts ...ts)
+{
+	return (std::count(std::begin(range), std::end(range), ts) + ...);
+}
+
+int main()
+{
+	using namespace std;
+
+	vector ivec{ 23, 4, 78, 12, 9, 2, 13 };
+	cout << matches(ivec, 4, 2, 13, 45, 120) << '\n';
+	cout << matches(ivec, 4, 2, 13, 45, 120) << '\n';
+	string str;
+
+	cout << "bir cumle girin: ";
+	getline(cin, str);
+	cout << matches(str, 'e', 'f', 'i') << '\n';
+}
