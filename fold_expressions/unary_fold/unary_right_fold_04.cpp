804736703c3ae871f82f6573df5d96ccc0bd99d6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-10 15:31:17 +0300

    Update unary_right_fold_04.cpp

--- fold_expressions/unary_fold/unary_right_fold_04.cpp
+++ fold_expressions/unary_fold/unary_right_fold_04.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 template<typename T, typename ...Args>
-bool all_in_range(const T& min, const T&max, Args && ...args)
+bool all_in_range(const T& min, const T& max, Args && ...args)
 {
 	return ((min <= std::forward<Args>(args) && max >= std::forward<Args>(args))  && ...);
 }
 
 int main()
 {
-	using namespace std;
+	boolalpha(std::cout);
 
-	cout << boolalpha << all_in_range(1, 20, 2, 4, 6, 7, 9) << "\n";
-	cout << boolalpha << all_in_range(10, 20, 12, 17, 25, 14) << "\n";
+	std::cout << all_in_range(1, 20, 2, 4, 6, 7, 9) << '\n';
+	std::cout << all_in_range(10, 20, 12, 17, 25, 14) << '\n';
 }
