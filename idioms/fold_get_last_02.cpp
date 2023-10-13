0425241969aa177b90a91bd7e1597b1abf5072a8
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-13 17:01:23 +0300

    Update fold_get_last_02.cpp

--- idioms/fold_get_last_02.cpp
+++ idioms/fold_get_last_02.cpp
@@ -10,7 +10,7 @@ constexpr auto func(auto... args)
 int main()
 {
     func(); // void func();
-    constexpr auto x1 = func(5);  /
+    constexpr auto x1 = func(5);  
     constexpr auto x2 = func(5, 1.2);
     constexpr auto x3 = func(5, 1.2, 3L);
     constexpr auto x4 = func(5, 1.2, 3L, "necati");
