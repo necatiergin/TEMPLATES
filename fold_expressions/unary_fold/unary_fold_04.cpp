dbde73f81ab0cca915a0d7c8fd4a04a44911db0a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-13 15:30:59 +0300

    Update unary_fold_04.cpp

--- fold_expressions/unary_fold/unary_fold_04.cpp
+++ fold_expressions/unary_fold/unary_fold_04.cpp
@@ -5,7 +5,7 @@
 #include <string>
 
 template<typename C, typename ...Ts>
-auto matches(const C& range, Ts ...ts)
+auto matches(const C& range, const Ts& ...ts)
 {
 	return (std::count(std::begin(range), std::end(range), ts) + ...);
 }
@@ -16,10 +16,9 @@ int main()
 
 	vector ivec{ 23, 4, 78, 12, 9, 2, 13 };
 	cout << matches(ivec, 4, 2, 13, 45, 120) << '\n';
-	cout << matches(ivec, 4, 2, 13, 45, 120) << '\n';
 	string str;
 
-	cout << "bir cumle girin: ";
+	cout << "enter a sentence: ";
 	getline(cin, str);
 	cout << matches(str, 'e', 'f', 'i') << '\n';
 }
