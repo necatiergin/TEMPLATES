3052a4affe1a59f24994c038df68ce23b4a041db
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-13 15:51:17 +0300

    Update unary_fold_06.cpp

--- fold_expressions/unary_fold/unary_fold_06.cpp
+++ fold_expressions/unary_fold/unary_fold_06.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <type_traits>
 
-auto fold_and(auto... args) 
+auto fold_and(auto... args)
 {
     return (args && ...);
 }
@@ -10,10 +11,17 @@ auto fold_or(auto... args)
     return (args || ...);
 }
 
+auto fold_comma(auto... args)
+{
+    return (args , ...);
+}
+
 int main()
 {
     boolalpha(std::cout);
 
     std::cout << fold_and() << '\n'; //true
     std::cout << fold_or() << '\n'; //false
+
+    static_assert(std::is_void_v<decltype(fold_comma())>);
 }
