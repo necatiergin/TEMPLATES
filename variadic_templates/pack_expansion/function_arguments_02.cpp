c5efe662cd30d6c3cad8cd1381633203fd8f14d0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-13 10:18:03 +0300

    Update function_arguments_02.cpp

--- variadic_templates/pack_expansion/function_arguments_02.cpp
+++ variadic_templates/pack_expansion/function_arguments_02.cpp
@@ -17,9 +17,9 @@ void call_sum(T... args)
 {
 	auto x1 = sum(args...);  // sum(1, 2, 3, 4, 5)
 	std::cout << "x1 = " << x1 << '\n';
-	auto x2 = sum(85, args...);
+	auto x2 = sum(85, args...); // sum(85, 1, 2, 3, 4, 5)
 	std::cout << "x2 = " << x2 << '\n';
-	auto x3 = sum(square(args)...);
+	auto x3 = sum(square(args)...);   //sum(square(1), square(2), square(3), square(4), square(5))
 	std::cout << "x3 = " << x3 << '\n';
 }
 
