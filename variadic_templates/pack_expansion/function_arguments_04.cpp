771cd1a0c06428b4811efde6e7e16a6f7dceb074
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-13 10:32:40 +0300

    Update function_arguments_04.cpp

--- variadic_templates/pack_expansion/function_arguments_04.cpp
+++ variadic_templates/pack_expansion/function_arguments_04.cpp
@@ -16,9 +16,11 @@ template <typename ...Ts>
 constexpr auto foo(Ts ...args)
 {
 	return mul(sum(args...) + args...);
+	//  mul(sum(1, 2, 4) + 1, sum(1, 2, 4) + 2, sum(1, 2, 4) + 4)
+	//  mul(8, 9, 11) 
 }
 
 int main()
 {
-	constexpr auto result = foo(3, 5, 6);
+	constexpr auto result = foo(1, 2, 4);
 }
