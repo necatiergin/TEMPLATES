674a29916558500f9a11784d624ec2d0023a8022
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-14 07:42:23 +0300

    Create is_09.cpp

---
+++ integer_sequence/is_09.cpp
@@ -0,0 +1,24 @@
+#include <tuple>
+
+template <typename ...Ts>
+void f(Ts &&...)
+{
+    //...
+}
+
+template <typename Tuple, std::size_t... I>
+void process_impl(const Tuple& t, std::index_sequence<I...>)
+{
+    f(std::get<I>(t)...);
+}
+
+template <typename Tuple>
+void process(const Tuple &t)
+{
+    process(t, std::make_index_sequence<std::tuple_size<Tuple>::value>());
+}
+
+int main()
+{
+    std::tuple(1, 'A', 2.3);
+}
