d60e75a157ad221ba1a296538ea61cf01dcc96a7
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-15 07:14:12 +0300

    Update ifc_05.cpp

--- if_constexpr/ifc_05.cpp
+++ if_constexpr/ifc_05.cpp
@@ -1,37 +1,39 @@
 #include <iterator>
 #include <iostream>
 
-//implementation for random access iterators
-template <typename Iter, typename Dist>
-void Advance_impl(Iter& pos, Dist n, std::random_access_iterator_tag)
-{
-	//std::cout << "random access iterator\n";
-	pos += n;
-}
+namespace details {
+	//implementation for random access iterators
+	template <typename Iter, typename Dist>
+	void Advance_impl(Iter& pos, Dist n, std::random_access_iterator_tag)
+	{
+		//std::cout << "random access iterator\n";
+		pos += n;
+	}
 
-//implementation for random bidirectional iterators
-template <typename Iter, typename Dist>
-void Advance_impl(Iter& pos, Dist n, std::bidirectional_iterator_tag)
-{
-	std::cout << "bidirectional iterator\n";
-	if (n >= 0) {
-		while (n--)
+	//implementation for random bidirectional iterators
+	template <typename Iter, typename Dist>
+	void Advance_impl(Iter& pos, Dist n, std::bidirectional_iterator_tag)
+	{
+		std::cout << "bidirectional iterator\n";
+		if (n >= 0) {
+			while (n--)
+				++pos;
+		}
+		else {
+			while (n++)
+				--pos;
+		}
+	}
+
+	//implementation for input iterators
+	template <typename Iter, typename Dist>
+	void Advance_impl(Iter& pos, Dist n, std::input_iterator_tag)
+	{
+		//std::cout << "input iterator\n";
+
+		while (n--) {
 			++pos;
-	}
-	else {
-		while (n--)
-			--pos;
-	}
-}
-
-//implementation for input iterators
-template <typename Iter, typename Dist>
-void Advance_impl(Iter& pos, Dist n, std::input_iterator_tag)
-{
-	//std::cout << "input iterator\n";
-
-	while (n--) {
-		++pos;
+		}
 	}
 }
 
@@ -39,7 +41,7 @@ template <typename Iter, typename Dist>
 void Advance(Iter& pos, Dist n)
 {
 	using Cat = typename std::iterator_traits<Iter>::iterator_category;
-	Advance_impl(pos, n, Cat{});
+	details::Advance_impl(pos, n, Cat{});
 }
 
 #include <vector>
