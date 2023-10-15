b15ee03c2ba527852cd5a6331713993c4c86b6c3
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-15 06:44:01 +0300

    Create object_count.cpp

---
+++ crtp/object_count.cpp
@@ -0,0 +1,53 @@
+#include <cstddef>
+
+template <typename T>
+class Counter {
+public:
+	Counter()
+	{
+		++ms_count_created;
+		++ms_count_alive;
+	}
+
+	Counter(const Counter&)
+	{
+		++ms_count_created;
+		++ms_count_alive;
+	}
+
+	~Counter()
+	{
+		--ms_count_alive;
+	}
+
+	static std::size_t how_many_created() { return ms_count_created; }
+	static std::size_t how_many_alive() { return ms_count_alive; }
+private:
+	static std::size_t ms_count_created;
+	static std::size_t ms_count_alive;
+};
+
+template <typename T>
+std::size_t Counter<T>::ms_count_created{};
+
+template <typename T>
+std::size_t Counter<T>::ms_count_alive{};
+
+
+class Neco : Counter<Neco> {
+public:
+	using Counter<Neco>::how_many_alive;
+	using Counter<Neco>::how_many_created;
+};
+
+#include <iostream>
+
+int main()
+{
+	Neco n1, n2, n3;
+	{
+		Neco n1, n2, n3;
+	}
+	std::cout << Neco::how_many_alive() << '\n';
+	std::cout << Neco::how_many_created() << '\n';
+}
