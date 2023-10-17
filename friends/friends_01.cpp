059e331732c41a7ffc9cc730af0c024dcd52aeb2
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-17 15:52:45 +0300

    Update friends_01.cpp

--- friends/friends_01.cpp
+++ friends/friends_01.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 template<typename T>
 class Nec {
 	template<typename U>
@@ -12,14 +14,23 @@ public:
 	template <typename U>
 	void afunc(Nec<U> nec)
 	{
-		nec.foo();
+		std::cout << typeid(*this).name() << '\n';
+		std::cout << typeid(decltype(nec)).name() << '\n';
+		std::cout << "--------------------------------------\n";
+		nec.foo(); //private member
 	}
 };
 
 int main()
 {
-	A<int> ax;
-	Nec<double> dnec;
+	A<int> a1;
+	A<double> a2;
+	
+	Nec<char> nec1;
+	Nec<float> nec2;
 
-	ax.afunc(dnec);
+	a1.afunc(nec1);
+	a1.afunc(nec2);
+	a2.afunc(nec1);
+	a2.afunc(nec2);
 }
