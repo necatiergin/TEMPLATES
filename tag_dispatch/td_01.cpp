96b3fe8cc7cddf6bdf2bc857131e55d273d79b35
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-19 19:03:05 +0300

    Update td_01.cpp

--- tag_dispatch/td_01.cpp
+++ tag_dispatch/td_01.cpp
@@ -1,25 +1,25 @@
 #include <type_traits>
 #include <iostream>
 
-template<typename T>
-void func_impl(T x, std::true_type)
-{
-	std::cout << "tam sayi turleri icin kod\n";
-}
+namespace detail {
+	template<typename T>
+	void func_impl(T x, std::true_type)
+	{
+		std::cout << "code for integer types\n";
+	}
 
-template<typename T>
-void func_impl(T x, std::false_type)
-{
-	std::cout << "tam sayi olmayan turler icin kod\n";
+	template<typename T>
+	void func_impl(T x, std::false_type)
+	{
+		std::cout << "code for non-integer types\n";
+	}
 }
-
 
 template<typename T>
 void func(T x)
 {
-	func_impl(x, std::is_integral<T>{});
+	detail::func_impl(x, std::is_integral<T>{});
 }
-
 
 int main()
 {
