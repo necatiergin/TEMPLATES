72bcedf46b54349b77e5869e23b5ec981f3330ec
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-19 14:48:15 +0300

    Update extern_template_declaration_01.cpp

--- instantiation/extern_template_declaration_01.cpp
+++ instantiation/extern_template_declaration_01.cpp
@@ -1,1 +1,42 @@
-soon
+///////////////////////////////
+// tpl_func.h
+
+template<typename T>
+void tpl_func()
+{
+    // body
+}
+
+///////////////////////////////
+// file_x.cpp
+
+//#include "tpl_func.h"
+
+void fx()
+{
+    //...
+    tpl_func<int>();
+}
+
+///////////////////////////////
+// file_y.cpp
+
+//#include "tpl_func.h"
+
+void fy()
+{
+    //...
+    tpl_func<int>();
+}
+
+/*
+> nm - g - C --defined - only * .o
+
+file_x.o:
+00000000 W void tpl_func<int>()
+00000000 T fx()
+
+file_y.o :
+    00000000 W void tpl_func<int>()
+    00000000 T fy()
+*/
