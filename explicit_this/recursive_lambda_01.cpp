0b225b76e2e4b7b0305bf2861f5fa559c3dff8d5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-22 04:25:25 +0300

    Update and rename et_06.cpp to recursive_lambda_01.cpp

--- explicit_this/recursive_lambda_01.cpp
+++ explicit_this/recursive_lambda_01.cpp
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-	auto f = [](this auto&& self, int n) {
+	auto factorial = [](this auto&& self, int n) {
 		if (n == 0)
 			return 1;
 		return n * self(n - 1);
 		};
 
-	std::cout << f(5) << '\n';
+	std::cout << factorial(5) << '\n';
 }
