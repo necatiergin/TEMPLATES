4d6cf34d92a6b657b38d12bb29a8614fa275e808
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-22 03:39:53 +0300

    Update object_count_1.cpp

--- crtp/object_count_1.cpp
+++ crtp/object_count_1.cpp
@@ -20,8 +20,8 @@ public:
 		--ms_count_alive;
 	}
 
-	static std::size_t how_many_created() { return ms_count_created; }
-	static std::size_t how_many_alive() { return ms_count_alive; }
+	static std::size_t get_created_count() { return ms_count_created; }
+	static std::size_t get_alive_count() { return ms_count_alive; }
 private:
 	inline static std::size_t ms_count_created{};
 	inline static std::size_t ms_count_alive{};
@@ -31,14 +31,14 @@ private:
 
 class Nec : Counter<Nec> {
 public:
-	using Counter<Nec>::how_many_alive;
-	using Counter<Nec>::how_many_created;
+	using Counter<Nec>::get_alive_count;
+	using Counter<Nec>::get_created_count;
 };
 
 class Erg : Counter<Erg> {
 public:
-	using Counter<Erg>::how_many_alive;
-	using Counter<Erg>::how_many_created;
+	using Counter<Erg>::get_alive_count;
+	using Counter<Erg>::get_created_count;
 };
 
 #include <iostream>
@@ -49,15 +49,15 @@ int main()
 	{
 		Nec n1, n2, n3;
 	}
-	std::cout << Nec::how_many_alive() << '\n';
-	std::cout << Nec::how_many_created() << '\n';
+	std::cout << Nec::get_alive_count() << '\n';
+	std::cout << Nec::get_created_count() << '\n';
 
 	Erg e1, e2;
 	{
 		Erg e1, e2, e3, e4;
 	}
-	std::cout << Erg::how_many_alive() << '\n';
-	std::cout << Erg::how_many_created() << '\n';
+	std::cout << Erg::get_alive_count() << '\n';
+	std::cout << Erg::get_created_count() << '\n';
 }
 /////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////
