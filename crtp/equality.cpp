decdbb7ea27e55796723aa7b9b218d503ac03c7e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-22 05:54:36 +0300

    Update equality.cpp

--- crtp/equality.cpp
+++ crtp/equality.cpp
@@ -4,20 +4,17 @@ struct equality {
 	{
 		return static_cast<const Der&>(*this);
 	}
+	
+	friend bool operator==(const equality& left, const equality& right)
+	{
+		return !(left.derived() < right.derived()) && !(right.derived() < left.derived());
+	}
+
+	friend bool operator!=(const equality& left, const equality& right)
+	{
+		return !(left == right);
+	}
 };
-
-template <typename Der>
-bool operator==(const equality<Der>& left, const equality<Der>& right)
-{
-	return !(left.derived() < right.derived()) && !(right.derived() < left.derived());
-}
-
-template <typename Der>
-bool operator!=(const equality<Der>& left, const equality<Der>& right)
-{
-	return !(left == right);
-}
-
 
 class Nec : public equality<Nec> {
 public:
