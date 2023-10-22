e590963a774b5636e99c1c5839d5c86840abaeb8
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-22 05:45:34 +0300

    Update and rename repeat_print.cpp to print_ntimes.cpp

--- crtp/print_ntimes.cpp
+++ crtp/print_ntimes.cpp
@@ -2,27 +2,29 @@
 #include <iostream>
 
 template<typename Printable>
-struct RepeatPrint
+struct Print_NTimes
 {
-    void repeat(unsigned int n) const
+    void repeat(std::size_t n) const
     {
         while (n-- > 0)
         {
-            static_cast<Printable const&>(*this).print();
+            static_cast<const Printable &>(*this).print();
+            //static_cast<const Printable *>(this)->print();
         }
     }
 };
 
-class Name : public RepeatPrint<Name>
+class Name : public Print_NTimes<Name>
 {
 public:
+    Name() = default;
     Name(std::string first_name, std::string last_name)
         : m_first_name(std::move(first_name))
         , m_last_name(std::move(last_name)) {}
 
     void print() const
     {
-        std::cout << m_first_name << ", " << m_last_name << '\n';
+        std::cout << m_first_name << ' ' << m_last_name << '\n';
     }
 
 private:
@@ -35,3 +37,7 @@ int main()
     Name nec("Necati", "Ergin");
     nec.repeat(10);
 }
+
+//------------------------------------------------------
+//------------------------------------------------------
+
