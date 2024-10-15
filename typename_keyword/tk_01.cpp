885301b02e27ee48d5d4ed9021532adb62bd212b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-15 18:13:46 +0300

    Update tk_01.cpp

--- typename_keyword/tk_01.cpp
+++ typename_keyword/tk_01.cpp
@@ -1,1 +1,16 @@
-soon
+template <class T, class U>
+void foo(T, U);
+
+template <typename T, typename U>
+void foo(T, U);
+
+template <typename T, class U>
+void foo(T, U);
+
+template <class T, typename U>
+void foo(T, U);
+
+int main()
+{
+	foo(1, 5L);
+}
