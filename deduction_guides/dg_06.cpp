d9ee4844ccfd0be06d6496c084e9df4e57eaa683
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-18 16:47:38 +0300

    Update dg_06.cpp

--- deduction_guides/dg_06.cpp
+++ deduction_guides/dg_06.cpp
@@ -10,8 +10,8 @@ explicit Nec(const char*)->Nec<std::string>;
 
 int main()
 {
-	//Nec n1 = { "neco" };	// geçersiz, deduction guide ignored
+	//Nec n1 = { "neco" };	// invalid, deduction guide ignored
 	Nec n2{ "neco" }; // Nec<std::string> n2{"neco"};
-	Nec n3 = Nec{ "hello" }; // geçerli
-	Nec n4 = { Nec{"hello"} }; // geçerli
+	Nec n3 = Nec{ "hello" }; // valid
+	Nec n4 = { Nec{"hello"} }; // valid
 }
