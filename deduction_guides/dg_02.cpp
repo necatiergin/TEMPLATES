90ae045ff34549b200949d4f2f626271a95b507a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-18 16:38:57 +0300

    Update dg_02.cpp

--- deduction_guides/dg_02.cpp
+++ deduction_guides/dg_02.cpp
@@ -12,5 +12,5 @@ Nec(T) -> Nec<T>; // deduction guide
 Nec x{5 }; // Nec<int> x{5};
 Nec y(3.3); // Nec<double> y(3.3);
 auto z = Nec{ 4L}; // auto z = Nec<long> {4L};
-//Nec* p = &x; //gecersiz
-//Nec n1{ 'A' }, n2{ 23 }; //gecersiz
+//Nec* p = &x; //invalid
+//Nec n1{ 'A' }, n2{ 23 }; //invalid
