bf98a7984aa230b98276bc2318a10347b7e55a77
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-28 14:43:07 +0300

    Update sa_03.cpp

--- static_assert/sa_03.cpp
+++ static_assert/sa_03.cpp
@@ -4,7 +4,7 @@ template <typename T>
 void Swap(T& x, T& y) noexcept
 {
     static_assert(std::is_copy_constructible_v<T>, "Swap template argument must be copy_constructible");
-    static_assert(std::is_copy_assignable_v<T>, "Swap template argument must be copy_assignible");
+    static_assert(std::is_copy_assignable_v<T>, "Swap template argument must be copy_assignable");
     static_assert(std::is_nothrow_copy_constructible_v<T> && std::is_nothrow_copy_assignable_v<T>, "Swap requires nothrow copy/assign");
     auto t = x;
     x = y;
