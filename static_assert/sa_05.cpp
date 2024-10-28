f99f6bae84593a2e43375a3364f276d9da997a35
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-28 14:48:13 +0300

    Update sa_05.cpp

--- static_assert/sa_05.cpp
+++ static_assert/sa_05.cpp
@@ -1,23 +1,23 @@
 #include <type_traits>
 #include <string>
 
-static_assert(sizeof(void*) == 4, "32-bit adresleme gerekli!");
-static_assert(sizeof(void*) == 8, "64-bit adresleme gerekli!");
+static_assert(sizeof(void*) == 4, "32-bits addressing is required!");
+static_assert(sizeof(void*) == 8, "64-bits addressing is required!");
 
 template <typename T, int Row, int Col>
 struct Matrix {
-	static_assert(Row > 0, "Satir sayisi pozitif bir deger olmali!");
-	static_assert(Col > 0, "Sutun sayisi pozitif bir deger olmali!");
-	static_assert(Row > Col, "Satir sayisi sutun sayisindan buyuk olmali!");
+	static_assert(Row > 0, "row number must be greater than 0");
+	static_assert(Col > 0, "column number must be greater than 0");
+	static_assert(Row > Col, "row number must be greater than column number!");
 	//...
 };
 
 int main()
 {
 
-	static_assert(sizeof(int) == sizeof(long int), "int ve long ayni sizeof degerinde olmali");
+	static_assert(sizeof(int) == sizeof(long int), "sizeof int and sizeof long must be equal");
 
 	Matrix<int, 10, 5> m1; //ok
-	Matrix<std::string, 3, 4> m2; //gecersiz
-	Matrix<double, 10, 0> m3; //gecersiz
+	Matrix<std::string, 3, 4> m2; //invalid
+	Matrix<double, 10, 0> m3; //invalid
 }
