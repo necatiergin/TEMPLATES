4eb1d732be664c3992055503126f00374c656f65
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-24 15:57:16 +0300

    Update sfinae_02.cpp

--- sfinae/basic/sfinae_02.cpp
+++ sfinae/basic/sfinae_02.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 
-
 template<typename T, unsigned N>
 std::size_t len(T(&)[N])
 {
@@ -19,16 +18,19 @@ std::size_t len(...)
 	return 0;
 }
 
-
 int main()
 {
 	int a[10];
-	std::cout << len(a); 
-	std::cout << len("tmp"); 
+	std::cout << len(a) << '\n';
+	
+	std::cout << len("tmp") << '\n';
+
 	std::vector<int> v;
-	std::cout << len(v); 
+	std::cout << len(v) << '\n';
+	
 	int* p{};
-	std::cout << len(p); 
+	std::cout << len(p) << '\n';
+	
 	std::allocator<int> x;
-	std::cout << len(x); 
+	//std::cout << len(x); //invalid
 }
