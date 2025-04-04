36ee643d9efb78160da49895e96ff0b8cd308bdf
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-04-04 13:35:16 +0300

    Update vt_06_1.cpp

--- variable_templates/vt_06_1.cpp
+++ variable_templates/vt_06_1.cpp
@@ -1,12 +1,8 @@
-#include <iostream>
-
 template <int N>
-bool is_positive{ N > 0 };
+constexpr bool is_positive{ N > 0 };
 
 int main()
 {
-	using namespace std;
-	
-	std::cout << is_positive<4> << '\n';
-	std::cout << is_positive<-5> << '\n';
+	static_assert(is_positive<10>);
+	static_assert(!is_positive<-5>);
 }
