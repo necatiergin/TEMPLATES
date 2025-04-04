0034d68d7d8c7997c13b5aa13c6492aebf5a99d5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-04-04 13:41:39 +0300

    Update vt_08.cpp

--- variable_templates/vt_08.cpp
+++ variable_templates/vt_08.cpp
@@ -1,10 +1,10 @@
 #include <cstddef>
 
 template <size_t VAL>
-constexpr size_t set_count= (VAL % 2) + set_count<(VAL / 2)>;
+constexpr size_t set_bit_count = (VAL % 2) + set_bit_count<(VAL / 2)>;
 
-template <> 
-constexpr size_t set_count<0> = 0;
+template <>
+constexpr size_t set_bit_count<0> = 0;
 
-constexpr size_t n1 = set_count<15>;
-constexpr size_t n2 = set_count<19>;
+static_assert(set_bit_count<15> == 4);
+static_assert(set_bit_count<19> == 3);
