aa6db04b08c8266f178c7946d6237ab0b092ac05
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-04-11 19:02:31 +0300

    Create sizeof_05.cpp

---
+++ variadic_templates/sizeof/sizeof_05.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+template<class...X, class...Y> 
+void foo(X...Arg1, int, int, Y...Arg2)
+{
+	std::cout << "sizeof X = " << sizeof ...(X) << '\n';
+	std::cout << "sizeof Y = " << sizeof ...(Y) << "\n\n";
+}
+
+int main()
+{
+	//X:(int, int, int), Y:(int, int, int, int, int) 
+	foo<int, int, int>(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+	// non trailing parameter pack should be empty	if there is no explicit template arguments
+	//X: empty, Y:(int, int, int, int, int)
+	foo(0, 5, 1, 2, 3, 4, 5);
+}
