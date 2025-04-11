d741f936ba576fd6f6f604b9022b0091d02d7a20
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-04-11 18:52:43 +0300

    Create multiple_01.cpp

---
+++ variadic_templates/pack_expansions/multiple_01.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template <typename ...Args>
+class Myclass {
+public:
+	Myclass(Args&& ...) {}
+};
+
+template <typename ...Ts>
+void func(Ts ...args)
+{
+	Myclass<Ts...>  m1;
+	Myclass<Ts...>  m2(args...);
+}
+
+template <typename ...Ts>
+void foo(Ts&&...args)
+{
+	Myclass<Ts...>  m3(std::forward<Ts>(args)...);
+}
+
+int main()
+{
+	std::string name{ "necati" };
+	std::vector vec{ 2, 6, 8, 1 };
+
+	foo(name, vec, 10);
+}
