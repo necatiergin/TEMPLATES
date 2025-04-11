e58eeb5af704d855c05d3b9b5d7f7fe4864afb04
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-04-11 18:28:27 +0300

    Create function_arguments_02.cpp

---
+++ variadic_templates/pack_expansions/function_arguments_02.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+template <typename ...Ts>
+constexpr auto sum(Ts ...args)
+{
+	return (0 + ... + args);
+}
+
+template <typename ...Ts>
+constexpr auto mul(Ts ...args)
+{
+	return (1 * ... * args);
+}
+
+template <typename ...Ts>
+constexpr auto foo(Ts ...args)
+{
+	return mul(sum(args...) + args...);
+	//  mul(sum(1, 2, 4) + 1, sum(1, 2, 4) + 2, sum(1, 2, 4) + 4)
+	//  mul(8, 9, 11) 
+}
+
+int main()
+{
+	constexpr auto result = foo(1, 2, 4);  //792
+}
