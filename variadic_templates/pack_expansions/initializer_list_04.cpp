4f808555e8f60b392ac3393e64cc63c323f60131
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-04-12 07:25:26 +0300

    Create initializer_list_04.cpp

---
+++ variadic_templates/pack_expansions/initializer_list_04.cpp
@@ -0,0 +1,12 @@
+#include <array>
+#include <type_traits>
+
+template <typename ...Ts>
+constexpr std::array<std::common_type_t<Ts...>, sizeof...(Ts)> make_array(Ts ...args) {
+    return { args...};
+}
+
+int main()
+{
+    auto ar = make_array(1, 3, 7);
+}
