aad3c8c92a1f44f41f37c1d75884d3a3570fb4e9
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-07-07 16:12:53 +0300

    Update sfinae_01.cpp

--- sfinae/basic/sfinae_01.cpp
+++ sfinae/basic/sfinae_01.cpp
@@ -4,25 +4,27 @@
 template<typename T, unsigned N>
 std::size_t len(T(&)[N])
 {
+	// std::cout << ("T(&)[N]\n");
 	return N;
 }
 
 template<typename T>
-typename T::size_type len(T const& t)
+typename T::size_type len(const T& t)
 {
+	// std::cout << ("const T&\n");
 	return t.size();
 }
 
 int main()
 {
 	int a[10]{};
-	std::cout << len(a);
-	std::cout << len("tmp");
+	std::cout << len(a) << '\n';
+	std::cout << len("tmp") << '\n';
 	int* p{};
-	//std::cout << len(p); //invalid
+	//std::cout << len(p) << '\n'; //invalid
 
 	std::allocator<int> x;
-	//std::cout << len(x); //invalid
+	//std::cout << len(x) << '\n'; //invalid
 	std::vector<int> ivec;
-	std::cout << len(ivec);	 // valid
+	std::cout << len(ivec) << '\n';
 }
