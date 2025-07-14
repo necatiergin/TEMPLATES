2f1dc9bb47090304edaa17cfcfc14d67509faa2d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-07-14 16:51:38 +0300

    Update tuple_example_01.cpp

--- variadic_templates/pack_expansions/tuple_example_01.cpp
+++ variadic_templates/pack_expansions/tuple_example_01.cpp
@@ -3,17 +3,19 @@
 #include <iostream>
 
 template<typename... Args>
-auto make_args_tuple(Args... args) 
+auto make_args_tuple(Args&&... args)
 {
-    return std::tuple<Args...>(args...); 
+    return std::tuple<Args...>(std::forward<Args>(args)...);
 }
 
 int main()
 {
     auto tp = make_args_tuple(1, 3, 5.6);
-    
+
     std::cout << typeid(tp).name() << '\n';
-    std::cout << get<0>(tp) << '\n';
-    std::cout << get<1>(tp) << '\n';
-    std::cout << get<2>(tp) << '\n';
+    std::cout << std::get<0>(tp) << '\n';
+    std::cout << std::get<1>(tp) << '\n';
+    std::cout << std::get<2>(tp) << '\n';
 }
+
+
