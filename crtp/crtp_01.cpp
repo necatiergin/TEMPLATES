3861e992b3cbe653ae19b439702da130a429b4c4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-07-23 15:06:32 +0300

    Update crtp_01.cpp

--- crtp/crtp_01.cpp
+++ crtp/crtp_01.cpp
@@ -4,15 +4,17 @@ template <typename Der>
 class Base {
 
 public:
-	void print_type()
+	void print_type() const
 	{
+		std::cout << typeid(*this).name() << '\n';
 		std::cout << typeid(Der).name() << '\n';
+		std::cout << "--------------------------------------\n";
 	}
 };
 
-class A : public Base<A> { };
-class B : public Base<B> { };
-class C : public Base<C> { };
+class A : public Base<A> {};
+class B : public Base<B> {};
+class C : public Base<C> {};
 
 
 int main()
